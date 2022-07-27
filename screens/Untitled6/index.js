53315666eba7b5f77259c6a987d9512cb93d851b
Rodrigo Gadea <rodrigo@crowdbotics.com>
2022-07-27 20:42:25 -0300

    Crowdbotics Studio: Update app screens

--- screens/Untitled6/index.js
+++ screens/Untitled6/index.js
@@ -1,3 +1,4 @@
+import { Text } from "react-native";
 import React from "react";
 import { View } from "react-native";
 
@@ -8,7 +9,127 @@ const Untitled6 = () => {
     position: 'relative',
     height: '100%'
   }}>
-      </View>;
+      <View style={{
+      left: 20,
+      top: 17,
+      position: "absolute",
+      height: 99,
+      width: 160,
+      backgroundColor: "#312fa1",
+      borderRadius: 0,
+      color: "#777777",
+      borderWidth: 2
+    }}></View><Text style={{
+      left: 20,
+      top: 50,
+      position: "absolute",
+      width: 160,
+      height: 50,
+      lineHeight: 14,
+      fontSize: 14,
+      borderRadius: 0,
+      textAlign: "center"
+    }}>Generated QR Code</Text><View style={{
+      left: 20,
+      top: 126,
+      position: "absolute",
+      height: 56,
+      width: 160,
+      backgroundColor: "#a02f2f",
+      borderRadius: 0,
+      color: "#777777",
+      borderWidth: 2
+    }}></View><View style={{
+      left: 20,
+      top: 195,
+      position: "absolute",
+      height: 31,
+      width: 160,
+      backgroundColor: "#2fa135",
+      borderRadius: 0,
+      color: "#777777",
+      borderWidth: 2
+    }}></View><Text style={{
+      left: 20,
+      top: 144,
+      position: "absolute",
+      width: 160,
+      height: 19,
+      lineHeight: 14,
+      fontSize: 14,
+      borderRadius: 0,
+      textAlign: "center"
+    }}>Emergency</Text><View style={{
+      left: 20,
+      top: 236,
+      position: "absolute",
+      height: 31,
+      width: 160,
+      backgroundColor: "#2fa135",
+      borderRadius: 0,
+      color: "#777777",
+      borderWidth: 2
+    }}></View><View style={{
+      left: 20,
+      top: 277,
+      position: "absolute",
+      height: 31,
+      width: 160,
+      backgroundColor: "#2fa135",
+      borderRadius: 0,
+      color: "#777777",
+      borderWidth: 2
+    }}></View><Text style={{
+      left: 20,
+      top: 201,
+      position: "absolute",
+      width: 160,
+      height: 19,
+      lineHeight: 14,
+      fontSize: 14,
+      borderRadius: 0,
+      textAlign: "center"
+    }}>Home</Text><Text style={{
+      left: 21,
+      top: 285,
+      position: "absolute",
+      width: 160,
+      height: 19,
+      lineHeight: 14,
+      fontSize: 14,
+      borderRadius: 0,
+      textAlign: "center"
+    }}>Medical History</Text><Text style={{
+      left: 20,
+      top: 243,
+      position: "absolute",
+      width: 160,
+      height: 19,
+      lineHeight: 14,
+      fontSize: 14,
+      borderRadius: 0,
+      textAlign: "center"
+    }}>Profile</Text><View style={{
+      left: 18,
+      top: 313,
+      position: "absolute",
+      height: 31,
+      width: 160,
+      backgroundColor: "#2fa135",
+      borderRadius: 0,
+      color: "#777777",
+      borderWidth: 2
+    }}></View><Text style={{
+      left: 19,
+      top: 319,
+      position: "absolute",
+      width: 160,
+      height: 19,
+      lineHeight: 14,
+      fontSize: 14,
+      borderRadius: 0,
+      textAlign: "center"
+    }}>Summary</Text></View>;
 };
 
 export default Untitled6;
