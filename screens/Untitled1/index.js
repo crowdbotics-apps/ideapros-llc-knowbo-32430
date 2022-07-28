cb3c67271fbce8d60e577757c6af1f0d0ecbf871
Rodrigo Gadea <rodrigo@crowdbotics.com>
2022-07-27 21:54:26 -0300

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,14 +1,38 @@
+import { Pressable } from "react-native";
+import { Text } from "react-native";
 import React from "react";
 import { View } from "react-native";
 
-const Untitled1 = () => {
+const Untitled1 = ({
+  navigation
+}) => {
   return <View style={{
-    backgroundColor: '#f0f0f1',
+    backgroundColor: "#f0f0f1",
     padding: 10,
-    position: 'relative',
-    height: '100%'
+    position: "relative",
+    height: "100%",
+    borderRadius: 0
   }}>
-      </View>;
+      <Pressable onPress={() => navigation.navigate("QR Scanner Screen")}><View style={{
+        left: -3,
+        top: 19,
+        position: "absolute",
+        height: 127,
+        width: 184,
+        backgroundColor: "#E4E4E4",
+        borderRadius: 0,
+        color: "#777777"
+      }}></View></Pressable><Text style={{
+      left: 8,
+      top: 70,
+      position: "absolute",
+      width: 182,
+      height: 50,
+      lineHeight: 14,
+      fontSize: 14,
+      borderRadius: 0,
+      textAlign: "center"
+    }}>QR Scanner </Text></View>;
 };
 
 export default Untitled1;
