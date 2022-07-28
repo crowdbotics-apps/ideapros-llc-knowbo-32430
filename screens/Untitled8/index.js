c2ad5bd2b23a25c4258401379ea0a9dac896e7a3
Rodrigo Gadea <rodrigo@crowdbotics.com>
2022-07-27 21:44:03 -0300

    Crowdbotics Studio: Update app screens

--- screens/Untitled8/index.js
+++ screens/Untitled8/index.js
@@ -1,14 +1,92 @@
+import { Text } from "react-native";
 import React from "react";
 import { View } from "react-native";
 
 const Untitled8 = () => {
   return <View style={{
-    backgroundColor: '#f0f0f1',
+    backgroundColor: "#f0f0f1",
     padding: 10,
-    position: 'relative',
-    height: '100%'
+    position: "relative",
+    height: "100%",
+    borderRadius: 0
   }}>
-      </View>;
+      <View style={{
+      left: 15,
+      top: 17,
+      position: "absolute",
+      height: 71,
+      width: 169,
+      backgroundColor: "#3e81aa",
+      borderRadius: 0,
+      color: "#777777"
+    }}></View><View style={{
+      left: 15,
+      top: 98,
+      position: "absolute",
+      height: 71,
+      width: 169,
+      backgroundColor: "#245f84",
+      borderRadius: 0,
+      color: "#777777"
+    }}></View><View style={{
+      left: 15,
+      top: 179,
+      position: "absolute",
+      height: 71,
+      width: 169,
+      backgroundColor: "#3e81aa",
+      borderRadius: 0,
+      color: "#777777"
+    }}></View><View style={{
+      left: 15,
+      top: 260,
+      position: "absolute",
+      height: 71,
+      width: 169,
+      backgroundColor: "#256994",
+      borderRadius: 0,
+      color: "#777777"
+    }}></View><Text style={{
+      left: 15,
+      top: 41,
+      position: "absolute",
+      width: 168,
+      height: 27,
+      lineHeight: 14,
+      fontSize: 14,
+      borderRadius: 0,
+      textAlign: "center"
+    }}>Medications Card</Text><Text style={{
+      left: 15,
+      top: 119,
+      position: "absolute",
+      width: 168,
+      height: 27,
+      lineHeight: 14,
+      fontSize: 14,
+      borderRadius: 0,
+      textAlign: "center"
+    }}>Conditions Card</Text><Text style={{
+      left: 17,
+      top: 208,
+      position: "absolute",
+      width: 168,
+      height: 27,
+      lineHeight: 14,
+      fontSize: 14,
+      borderRadius: 0,
+      textAlign: "center"
+    }}>Allergies Card</Text><Text style={{
+      left: 15,
+      top: 287,
+      position: "absolute",
+      width: 168,
+      height: 27,
+      lineHeight: 14,
+      fontSize: 14,
+      borderRadius: 0,
+      textAlign: "center"
+    }}>DNR/Directives Card</Text></View>;
 };
 
 export default Untitled8;
--- screens/Untitled9/index.js
+++ screens/Untitled9/index.js
@@ -1,3 +1,4 @@
+import { Text } from "react-native";
 import React from "react";
 import { View } from "react-native";
 
@@ -8,7 +9,62 @@ const Untitled9 = () => {
     position: 'relative',
     height: '100%'
   }}>
-      </View>;
+      <Text style={{
+      left: 10,
+      top: 16,
+      position: "absolute",
+      width: 175,
+      height: 39,
+      lineHeight: 14,
+      fontSize: 14,
+      borderRadius: 0
+    }}>- Patient name</Text><Text style={{
+      left: 10,
+      top: 65,
+      position: "absolute",
+      width: 175,
+      height: 39,
+      lineHeight: 14,
+      fontSize: 14,
+      borderRadius: 0
+    }}>- DOB</Text><Text style={{
+      left: 10,
+      top: 114,
+      position: "absolute",
+      width: 175,
+      height: 39,
+      lineHeight: 14,
+      fontSize: 14,
+      borderRadius: 0
+    }}>- Medications: Names and details about medicaments user uses</Text><Text style={{
+      left: 10,
+      top: 169,
+      position: "absolute",
+      width: 175,
+      height: 39,
+      lineHeight: 14,
+      fontSize: 14,
+      borderRadius: 0
+    }}>- Conditions: List of conditions user checked</Text><Text style={{
+      left: 10,
+      top: 214,
+      position: "absolute",
+      width: 175,
+      height: 39,
+      lineHeight: 14,
+      fontSize: 14,
+      borderRadius: 0
+    }}> - Allergies: List of allergies user entered</Text><Text style={{
+      left: 10,
+      top: 263,
+      position: "absolute",
+      width: 175,
+      height: 39,
+      lineHeight: 14,
+      fontSize: 14,
+      borderRadius: 0
+    }}> - DNR: Text entered on DNR card
+    </Text></View>;
 };
 
 export default Untitled9;
