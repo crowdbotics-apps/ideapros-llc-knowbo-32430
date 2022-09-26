12644a8f321f22ab4164b1d74cf24d0e7220f4ab
Aline Tavares <alaisgomes@gmail.com>
2022-09-26 14:58:13 -0400

    Fix onboarding module; fix app menu modules

--- modules/onboarding/index.js
+++ modules/onboarding/index.js
@@ -1,63 +1,67 @@
 import React from "react";
-import { View, Image, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Image,
+  Text,
+  StyleSheet,
+} from "react-native";
 import { slides } from "./slides";
 import AppIntroSlider from 'react-native-app-intro-slider';
-const REDIRECT_SCREEN_NAME = 'LoginAndSignup177769';
 
-const Onboarding = ({
-  navigation
-}) => {
-  const renderItem = ({
-    item
-  }) => {
-    return <View style={styles.jLBDJFoe}>
+const REDIRECT_SCREEN_NAME = 'Signupscreen'
+
+
+const Onboarding = ({ navigation }) =>  {
+
+  const renderItem = ({ item }) => {
+    return (
+      <View style={[styles.slide, { backgroundColor: item.backgroundColor }]}>
         <Text style={styles.title}>{item.title}</Text>
         <Image style={styles.image} source={item.image} />
         <Text style={styles.text}>{item.text}</Text>
-      </View>;
-  };
+      </View>
+    );
+  }
 
   const onDone = () => {
-    navigation.navigate(REDIRECT_SCREEN_NAME);
-  };
+    navigation.navigate(REDIRECT_SCREEN_NAME)
+  }
 
-  return <View style={styles.ZCekTIVf}>
+
+  return (
+    <View style={{ flex: 1, }}>
       <AppIntroSlider renderItem={renderItem} data={slides} onDone={onDone} />
-    </View>;
-};
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginHorizontal: 16
+    marginHorizontal: 16,
   },
   slide: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   image: {
     width: 320,
     height: 320,
-    marginVertical: 32
+    marginVertical: 32,
   },
   text: {
     color: 'rgba(255, 255, 255, 0.8)',
-    textAlign: 'center'
+    textAlign: 'center',
   },
   title: {
     fontSize: 22,
     color: 'white',
-    textAlign: 'center'
+    textAlign: 'center',
   },
-  jLBDJFoe: {
-    backgroundColor: "item.backgroundColor"
-  },
-  ZCekTIVf: {
-    flex: 1
-  }
 });
+
 export default {
   title: "Onboarding Module",
   navigator: Onboarding
-};
+}
